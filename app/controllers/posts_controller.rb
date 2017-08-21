ec472c7926cdfaff052d8679e0ad7ed2d624088b
Wen-Tien Chang <ihower@gmail.com>
2017-08-21 22:45:49 +0800

    Counter cache

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,6 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.includes(:user, :liked_users, { :visible_comments => :user } ).page(params[:page])
-
-    post_ids = @posts.map{ |p| p.id }
-    @subscriptions_count = Post.where( :id => post_ids).joins(:subscriptions).group("posts.id").count
   end
 
   def show
