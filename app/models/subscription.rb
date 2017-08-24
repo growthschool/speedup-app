9d9f1ab68c31516df975883779c70f970f5397a5
lanqiu1986 <lisuji1986@163.com>
2017-08-24 15:12:51 +0800

    completed

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,8 @@
 class Subscription < ApplicationRecord
 
-  belongs_to :post
+  belongs_to :post, :counter_cache => true
+  #or -- belongs_to :post, :counter_cache => "subscriptions_count"
   belongs_to :user
+
 
 end
