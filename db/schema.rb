f81874c3ebb85135fe4066bbfd3cab89d4715fc8
meganho <bbeeaanns@gmail.com>
2017-08-24 16:51:17 +0200

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 20170821133300) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "date"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
