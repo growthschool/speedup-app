80675ebdc98eaa2739cd8f12297113234bef0c25
cwy007 <chanweiyan007@gmail.com>
2017-09-08 00:16:47 +0800

    find_each

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170821133300) do
+ActiveRecord::Schema.define(version: 20170907161342) do
 
   create_table "comments", force: :cascade do |t|
     t.string "status", default: "public"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20170821133300) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "date"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
