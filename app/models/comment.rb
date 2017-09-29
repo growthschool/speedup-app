d443969ee4645bf4d3a41aae4cdab800c3114f1f
Antarts <m.dream@foxmail.com>
2017-09-29 21:52:21 +0800

    show subscriptions count

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,5 +4,6 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   scope :visible, -> { where( :status => "public") }
+  scope :mine, -> (user){ where( :status => "private", :user_id => user.id ) }
 
 end
